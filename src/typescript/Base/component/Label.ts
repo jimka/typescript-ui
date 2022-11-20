8d661f0a3ffbda6d5a2a3123a3c60ef47d65e5ef
Jimmy Karlsson <jimmy.karlsson@gmail.com>
2022-11-20 15:30:55 +0100

    Make the Label-component an actual <label>

--- src/typescript/Base/component/Label.ts
+++ src/typescript/Base/component/Label.ts
@@ -1,151 +1,35 @@
-import { Component } from "../Component.js";
-import { Fit } from "../layout/Fit.js";
 import { Text } from "./Text.js";
-import { AnchorType } from "../layout/AnchorType.js";
 
-export class Label extends Component {
+export class Label extends Text {
 
-    private textComponent: Text;
+    forId: string;
 
-    constructor(text?: String) {
-        super();
+    constructor(text? : string, forId : string = "") {
+        super("label", text);
 
-        this.setLayoutManager(new Fit());
-        this.textComponent = new Text("span", text);
-        this.addComponent(this.textComponent, {
-            //fill: FillType.NONE,
-            anchor: AnchorType.CENTER
-        });
-
-        this.setInsets(null);
-        this.textComponent.setPointerEvents("none");
+        this.forId = forId;
     }
 
-    getSize() {
-        return this.getPreferredSize();
+    public getForId() : string {
+        return this.forId;
     }
 
-    getPreferredSize() {
-        let ps = super.getPreferredSize();
-        if (ps) {
-            return ps;
+    public setForId(id : string) {
+        this.forId = id;
+
+        let element = this.getElement();
+        if (!element) {
+            return;
         }
 
-        let size = this.textComponent.getSize();
-        if (!size) {
-            return null;
-        }
-
-        let insets = this.getInsets();
-        let borderSize = this.getBorderSize();
-
-        size.width = size.width + (insets ? (insets.getLeft() + insets.getRight()) : 0) + (borderSize ? (borderSize.left + borderSize.right) : 0);
-        size.height = size.height + (insets ? (insets.getTop() + insets.getBottom()) : 0) + (borderSize ? (borderSize.top + borderSize.bottom) : 0);
-
-        return size;
+        element.textContent = id;
     }
 
-    getMinSize() {
-        return this.getPreferredSize();
-    }
+    protected render() {
+        let element = <HTMLLabelElement>super.render();
 
-    getText() {
-        return this.textComponent.getText();
-    }
+        element.htmlFor = this.forId;
 
-    setText(value: String) {
-        this.textComponent.setText(value);
-    }
-
-    getTextAlign() {
-        return this.textComponent.getTextAlign();
-    }
-
-    setTextAlign(value: string) {
-        this.textComponent.setTextAlign(value);
-    }
-
-    getTextShadow() {
-        return this.textComponent.getTextShadow();
-    }
-
-    setTextShadow(value: string) {
-        this.textComponent.setTextShadow(value);
-    }
-
-    getFontFamily() {
-        return this.textComponent.getFontFamily();
-    }
-
-    setFontFamily(value: string) {
-        this.textComponent.setFontFamily(value);
-    }
-
-    getFontKerning() {
-        return this.textComponent.getFontKerning();
-    }
-
-    setFontKerning(value: string) {
-        this.textComponent.setFontKerning(value);
-    }
-
-    getFontSize() {
-        return this.textComponent.getFontSize();
-    }
-
-    setFontSize(value: number) {
-        this.textComponent.setFontSize(value);
-    }
-
-    getFontSizeAdjust() {
-        return this.textComponent.getFontSizeAdjust();
-    }
-
-    setFontSizeAdjust(value: string) {
-        this.textComponent.setFontSizeAdjust(value);
-    }
-
-    getFontStretch() {
-        return this.textComponent.getFontStretch();
-    }
-
-    setFontStretch(value: string) {
-        this.textComponent.setFontStretch(value);
-    }
-
-    getFontStyle() {
-        return this.textComponent.getFontStyle();
-    }
-
-    setFontStyle(value: string) {
-        this.textComponent.setFontStyle(value);
-    }
-
-    getFontVariant() {
-        return this.textComponent.getFontVariant();
-    }
-
-    setFontVariant(value: string) {
-        this.textComponent.setFontVariant(value);
-    }
-
-    getFontWeight() {
-        return this.textComponent.getFontWeight();
-    }
-
-    setFontWeight(value: string) {
-        this.textComponent.setFontWeight(value);
-    }
-
-    getLineHeight() {
-        return this.textComponent.getLineHeight();
-    }
-
-    setLineHeight(value: number) {
-        this.textComponent.setLineHeight(value);
-    }
-
-    select(start: number, end: number) {
-        this.textComponent.select(start, end);
+        return element;
     }
 }
