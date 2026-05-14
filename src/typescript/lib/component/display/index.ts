0e7bfd16952de8806e927be8a99cceef4ae8176d
Jimmy Karlsson <jimmy.karlsson@gmail.com>
2026-05-14 15:39:45 +0200

    Retire FontAwesome
    Delete src/typescript/lib/component/display/FontAwesomeIcon.ts and
    the matching FontAwesomeIcon / FontAwesomeIconOptions exports from
    the display barrel. Remove the @fortawesome/fontawesome-free peer
    dependency (and its peerDependenciesMeta block) from package.json.
    Drop the <script src=\".../fontawesome/js/all.js\"></script> tags
    from index.html and src/resources/index.html. Delete the entire
    src/resources/Base/script/fontawesome/ asset tree (CSS, JS, less,
    scss, sprites, SVGs, webfonts, metadata).
    
    Update the stale \"FontAwesome code\" JSDoc reference in MenuItem
    to plain \"Unicode character\". Move the implementation plan from
    plans/ to plans/implemented/.

--- src/typescript/lib/component/display/index.ts
+++ src/typescript/lib/component/display/index.ts
@@ -4,8 +4,6 @@ export { Header } from '~/component/display/Header.js';
 export type { HeaderOptions } from '~/component/display/Header.js';
 export { Image } from '~/component/display/Image.js';
 export type { ImageOptions } from '~/component/display/Image.js';
-export { FontAwesomeIcon } from '~/component/display/FontAwesomeIcon.js';
-export type { FontAwesomeIconOptions } from '~/component/display/FontAwesomeIcon.js';
 export { Glyph } from '~/component/display/Glyph.js';
 export type { GlyphOptions } from '~/component/display/Glyph.js';
 export { IconText } from '~/component/display/IconText.js';
